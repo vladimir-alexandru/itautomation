828a43ff105de018affeec60e3df3427d8416acb
chase1009 <227479884+chase1009@users.noreply.github.com>
2025-10-20 21:13:05 -0400

    session_tracker_v1.0.py finalized both Windows & Linux.

--- session_tracker/session_tracker_v1.0.py
+++ session_tracker/session_tracker_v1.0.py
@@ -6,15 +6,7 @@ import re
 
 date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-def command():
-    if platform.system().lower().startswith("win"):
-        cmd = "query user"
-    else:
-        cmd = "who"
-    return cmd
-
-def main():
-    cmd = command()
+def win_system(cmd):
     filename = f"session_tracker_{date}.txt"
     try:
         result = subprocess.run(cmd, capture_output=True, text=True, check=False, timeout=5)
@@ -41,6 +33,44 @@ def main():
         print("Error:", e)
         sys.exit(1)
 
+def linux_system(cmd):
+    filename = f"session_tracker_{date}.txt"
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=False, timeout=5)
+        username = result.stdout.split()[0]
+
+        with open(filename, "w") as f:
+            f.write("=== USER CHECK ===\n")
+            f.write(f"Time: {datetime.datetime.now().strftime('%Y-%m-%d | %H:%M:%S')}\n")
+            f.write(f"Command: {cmd}\n")
+            f.write("-------\n")
+            if username:
+                f.write(f"Username: {username}\n")
+            else:
+                f.write("No output was returned.\n")
+            if result.stderr:
+                f.write("\n")
+                f.write("Errors:\n")
+                f.write(f"{result.stderr.strip()}\n")
+            f.write("")
+            f.write(f"Exit Code: {result.returncode}\n")
+            f.write("==================\n")
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def command():
+    if platform.system().lower().startswith("win"):
+        cmd = "query user"
+        win_system(cmd)
+    else:
+        cmd = "who"
+        linux_system(cmd)
+
+def main():
+    command()
+    
 if __name__ == "__main__":
     main()
   
